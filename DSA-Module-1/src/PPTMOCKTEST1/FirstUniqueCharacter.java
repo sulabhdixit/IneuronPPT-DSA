78497ec48568be17b419f557fb96a3fadd3a4139
delllaptop <delllaptop@DESKTOP-9BUMFC0>
2023-06-05 23:52:21 +0530

    PPT mock test 1 code solution 2

---
+++ DSA-Module-1/src/PPTMOCKTEST1/FirstUniqueCharacter.java
@@ -0,0 +1,34 @@
+package PPTMOCKTEST1;
+
+public class FirstUniqueCharacter {
+	public int firstUniqChar(String s) {
+		int len = s.length();
+
+		if (len == 0) {
+			return -1;
+		}
+
+		int[] arr = new int[26];
+
+		for (char c : s.toCharArray()) {
+			arr[c - 'a']++;
+		}
+
+		for (int i = 0; i < len; i++) {
+			if (arr[s.charAt(i) - 'a'] == 1) {
+				return i;
+			}
+		}
+
+		return -1;
+
+	}
+
+	public static void main(String[] args) {
+		String s="leetcode";
+		FirstUniqueCharacter f=new FirstUniqueCharacter();
+		int n=f.firstUniqChar(s);
+		System.out.println(n);
+	}
+
+}
